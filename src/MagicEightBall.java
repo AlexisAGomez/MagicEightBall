2ae2a35ac68ddf36572dae3c673258d3fd3f1067
PrCorporation <alexis.bepa@gmail.com>
2020-05-18 13:32:47 -0500

    Commit

--- src/MagicEightBall.java
+++ src/MagicEightBall.java
@@ -1,8 +1,6 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+
+import java.util.*;
 
 /*
 This method, MagicEightBall, outputs a randomized response when the 'ask()' method is invoked to console.
@@ -15,73 +13,88 @@ This method, MagicEightBall, outputs a randomized response when the 'ask()' meth
 public class MagicEightBall {
 
     private String question;
+    private String response;
     private boolean repeatquestion;
     public repeatQuestion repeatQuestion = new repeatQuestion();
-    private ArrayList<String> questions = new ArrayList<>();
     private final ArrayList<String> responses = new ArrayList<>(Arrays.asList(
             "Yes","No","Ask Another Time","Perhaps","I Plead the Fifth","You Don't Have The Rights To Ask This Question",
             "Definitely","Of Course","Of Course Not","Don't Get Your Hopes Up","I Wouldn't Be Surprised"
     ));
 
     public MagicEightBall() {
-
     }
 
     public String getQuestion() {
         return question;
     }
 
+    public String getAnswer() {return response;}
+
+    public void setRandomResponse() {
+        this.response = getResponse();
+    }
+
+// Sets Question and Answer for the MagicEightBallObject
     public void setQuestion(String question) {
         this.question = question;
-    }
-
-    public ArrayList<String> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(ArrayList<String> questions) {
-        this.questions = questions;
+        setRandomResponse();
     }
 
     private String getResponse() {
         return responses.get(new Random().nextInt(responses.size()));
     }
 
-    public void ask(String question) {
+    public String ask(String question) {
         if(repeatquestion) {
             if (!question.contains("?")) {
-                System.out.println(question + "?" + " \n" + getResponse());
+//                System.out.println(question + "?" + " \n" + getResponse());
+                this.question = question + "?";
+                return question + "?" + " \n" + getResponse();
             } else {
-                System.out.println(question + " \n" + getResponse());
+//                System.out.println(question + " \n" + getResponse());
+                this.question = question;
+                return question + " \n" + getResponse();
             }
         } else {
-            System.out.println(getResponse());
+//            System.out.println(getResponse());
+            return getResponse();
         }
 
     }
-    public void ask(ArrayList<String> questions) {
+
+    public String ask(String question, boolean containsQuestion) {
+        boolean temp = repeatquestion;
+        if(containsQuestion) {
+            repeatquestion = true;
+            return ask(question);
+        }
+            return getResponse();
+
+    }
+
+    public ArrayList<MagicEightBall> ask(ArrayList<String> questions) {
+
+        ArrayList<MagicEightBall> list = new ArrayList<>();
+
         questions.forEach(a -> {
-            if(repeatquestion) {
+
                 if (!a.contains("?")) {
-                    System.out.println(a + "?" + "\n" + getResponse());
+                    list.add(new MagicEightBall(a + "?"));
+//                    System.out.println(a + "?" + "\n" + getResponse());
                 } else {
-                    System.out.println(a + "\n" + getResponse());
+                    list.add(new MagicEightBall(a));
+//                    System.out.println(a + "\n" + getResponse());
                 }
-            } else {
-                System.out.println(getResponse());
-            }
+
         } );
+        return list;
     }
 
     public MagicEightBall(String question) {
         this.question = question;
-        ask(question);
+        setRandomResponse();
     }
 
-    public MagicEightBall(ArrayList<String> questions) {
-        this.questions = questions;
-        ask(questions);
-    }
 
     class repeatQuestion{
 
@@ -101,16 +114,31 @@ public class MagicEightBall {
 
 class test {
 
-    public static void main(String[] args) {
-        MagicEightBall magic8ball = new MagicEightBall();
-        magic8ball.repeatQuestion.setFalse();
-        while(true) {
-            Scanner in = new Scanner(System.in);
-            String Question = in.nextLine();
-            if(Question.toLowerCase().equals("exit")) break;
-            magic8ball.ask(Question);
+//    public static void main(String[] args) {
+//        MagicEightBall magic8ball = new MagicEightBall();
+//        magic8ball.repeatQuestion.setFalse();
+//        while(true) {
+//            Scanner in = new Scanner(System.in);
+//            String Question = in.nextLine();
+//            if(Question.toLowerCase().equals("exit")) break;
+//            System.out.println(magic8ball.ask(Question));
+//
+//        }
+//    }
 
-        }
+    public static void main(String[] args) {
+
+        ArrayList<String> questions = new ArrayList<>(Arrays.asList(
+                "What is the meaning of life?","What is 2+2?","What is Pi"
+        ));
+
+        MagicEightBall ball = new MagicEightBall();
+        ArrayList<MagicEightBall> responses = ball.ask(questions);
+
+        responses.forEach(a -> {
+            System.out.println(a.getQuestion() + "\n" + a.getAnswer() );
+        });
+
     }
 
 
